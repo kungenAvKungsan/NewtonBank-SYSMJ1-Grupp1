30d46d5e5573f254593fcbeda22eac25afeea3bc
iliemar <iliemar76@yahoo.se>
2015-11-11 19:14:08 +0100

    New 2

--- Customer.java
+++ Customer.java
@@ -5,23 +5,23 @@ import java.util.ArrayList;
 public class Customer {
 	private ArrayList<SavingsAccount>accounts;
 	private static String name;
-	private long pNr;
+	private static long pNr;
 	
 	public Customer(String name, long pNr, ArrayList<SavingsAccount> accounts){
-		this.name = name;
+		Customer.name = name;
 		this.pNr = pNr;
 		this.accounts = accounts;
 	}
 	
 	public static void setName(String name){
-		name = name;
+		name = Customer.name;
 	}
 	public static String getName(){
 		return name;
 		
 	}
 	public static void setPrsNbr(long pNr){
-		pNr = pNr;
+		pNr = Customer.pNr;
 	}
 	public long pNr(){
 		return pNr;

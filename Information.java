543985f685640ec99ecdf58578b1896c69dcf079
Nesho <kevin.nemec94@gmail.com>
2015-11-11 12:17:22 +0100

    Början på databas klassen.

---
+++ Information.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Information
+{
+	private ArrayList<String> customers;
+	public Information()
+	{
+		customers = new ArrayList<>();
+	}
+	
+	public void addCustomer(String n)
+	{
+		customers.add(n);
+	}
+	public ArrayList<String> getCustomers()
+	{
+		return customers;
+	}
+}

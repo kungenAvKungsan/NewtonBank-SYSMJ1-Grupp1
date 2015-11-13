110f16247c110aa968f8506d2fda498c9eb95237
viktorsorensen <viktor.sorensen87@outlook.com>
2015-11-13 18:39:18 +0100

    Update BankLogic.java

--- BankLogic.java
+++ BankLogic.java
@@ -13,4 +13,15 @@ public class BankLogic {
         }
         return custStrings;
     }
-}
+
+    public boolean addCustomer(String name, long pNr) {
+
+        for (Customer customer : customers) {
+            if (customer.pNr() == pNr)
+                return false;
+        }
+            Customer newCust = new Customer(name, pNr, null);
+            customers.add(newCust);
+            return true;
+        }
+    }
